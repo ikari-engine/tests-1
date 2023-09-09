60bbd26b51266c85d9bfb7f1362a5e13cd1e120a
lengors <24527258+lengors@users.noreply.github.com>
2023-09-09 12:53:31 +0100

    feat(subtraction): added subtraction to library

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,14 @@
 export function add(x: number, y: number): number {
   return x + y;
 }
+
+/**
+ * Subtract two numbers together
+ *
+ * @param x - The first number
+ * @param y - The second number
+ * @returns The subtraction of the two numbers
+ */
+export function sub(x: number, y: number): number {
+  return x - y;
+}
